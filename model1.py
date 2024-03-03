4617be1612d359d2aed3ca783b0facaec58b5039
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2024-03-03 12:07:06 +0530

    Add files via upload

--- model1.py
+++ model1.py
@@ -17,10 +17,22 @@ model = keras.Sequential([
     keras.layers.Dense(10, activation="softmax")
 ])
 
+
 model.compile(optimizer="adam", loss = "sparse_categorical_crossentropy", metrics=["accuracy"])
 
 model.fit(train_images, train_labels, epochs=5)
 
-test_loss, test_acc = model.evaluate(test_images, test_labels)
+# test_loss, test_acc = model.evaluate(test_images, test_labels)
 
-print("Tested Accuracy = ", test_acc)
+# print("Tested Accuracy = ", test_acc)
+
+prediction = model.predict(test_images)
+
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel("Actual:" + class_names[test_labels[i]])
+    plt.title("Prediction" + class_names[np.argmax(prediction[i])])
+    plt.show()
+
+    
